ba4cfca356486e2caab501ee1fd96609e9d2663d
UTakuto <taku.0307.ue@icloud.com>
2024-09-20 15:27:11 +0900

    Refactor FetchData component to include type annotation for data state

--- src/components/FetchData.tsx
+++ src/components/FetchData.tsx
@@ -1,9 +1,16 @@
 import { useEffect, useState } from "react";
 import { fetchPosts } from "./apiClient";
 
+type Post = {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+};
+
 export default function FetchData() {
     //api dataをここに保存
-    const [data, setData] = useState<any[]>([]);
+    const [data, setData] = useState<Post[]>([]);
 
     //useEffectを使って、コンポーネントがマウントされた時にfetchPosts関数を呼び出します
     //からの依存配列[]を渡しているので、この処置はコンポーネントの最初の描画時に一度だけ実行されます。
